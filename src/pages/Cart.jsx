46b7a4f6d2e48373a50b1517b95a390390ff138a
GhassanT21 <ghassantermsani@gmail.com>
2025-11-23 14:45:44 +0200

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -74,7 +74,7 @@ export default function Cart() {
             Clear Cart
           </button>
           <button className="bg-indigo-600 text-white px-4 py-2 rounded text-sm">
-            Checkout (Demo)
+            Checkout
           </button>
         </div>
       </div>
